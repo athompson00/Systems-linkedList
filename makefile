5a285d74369d3a794ad60c350875beff68d15657
athompson00 <athompson00@stuy.edu>
2019-10-27 22:26:39 -0400

    wrote print_list and insert_front. Need to test

--- makefile
+++ makefile
@@ -1,14 +1,16 @@
 all: main.o create.o
-	gcc -o program main.o create.o
+	gcc -o program main.o node.o
+
+node.o: node.c headers.h
+		gcc -c node.c
 
 main.o: main.c headers.h
 	gcc -c main.c
 
-create.o: create.c headers.h
-	gcc -c create.c
 
 run:
 		./program
 
 clean:
 		rm *.o
+		rm program
---
+++ node.c
@@ -0,0 +1,26 @@
+#include <time.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include "headers.h"
+
+
+
+void print_list(struct node *first){
+  printf("[ %d ", first->i);
+  struct node *new = first->next;
+
+  while (new != NULL){
+    printf("%d ", new->i);
+    new = new->next;
+  }
+
+  printf("]\n")
+}
+
+struct node * insert_front(struct node *first, int i){
+  struct node *newFirst = malloc(sizeOf(struct node));
+  newFirst->next = first;
+  newFirst->i = i;
+  return newFirst;
+}
